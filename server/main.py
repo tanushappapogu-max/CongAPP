tanushappapogu-max/Pulse-Point-

Integrate nvidia/LocateAnything-3B as primary detection engine
- Add server/locate_model.py: lazy-loads LocateAnything-3B via
  transformers + qwen_vl_utils; parses bbox_2d JSON output; handles
  both [x1,y1,x2,y2] and swapped coordinate ordering; falls back
  gracefully if model unavailable or CUDA missing
- Update /detect endpoint: tries LocateAnything first (open-vocab,
  any target in natural language), falls back to PulsePointNet
  (~45 indoor classes via ImageNet mapping)
- /health now reports locate_anything availability
- Add transformers>=4.57.1, accelerate, qwen_vl_utils to requirements
- Dockerfile pre-warms LocateAnything-3B weights at build time
- Frontend server.js forwards model name from response
- Stats bar shows LA-3B vs PPN label depending on which ran
- Reset serverModel state on scanner start/stop

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01E31PbFJsjGfQDzWQDBjkyd

server/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI, UploadFile, File, Form, Body
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import time
+
+from locate_model import locate_object, is_available as locate_available
 from model import predict, INDOOR_OBJECT_ONTOLOGY as INDOOR_OBJECTS
 from text_model import classify_text, retrain
 from tea_dataset import TEA_TYPES, FLAVOR_LABELS, QUALITY_TIERS
 
-app = FastAPI(title="Pulse Point Vision + Tea Text API")
+app = FastAPI(title="Pulse Point Vision API")
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,7 +20,11 @@ app.add_middleware(
 
 @app.get("/health")
 async def health():
-    return {"status": "ok", "objects": len(INDOOR_OBJECTS)}
+    return {
+        "status": "ok",
+        "locate_anything": locate_available(),
+        "fallback_objects": len(INDOOR_OBJECTS),
+    }
 
 
 @app.get("/objects")
@@ -40,11 +46,18 @@ async def detect(
             content={"error": "Image too large. Max 10MB."},
         )
 
-    result = predict(image_bytes, target_name=target if target else None)
+    result = None
+
+    # ── Primary: LocateAnything-3B (open-vocabulary, any target) ──
+    if target:
+        result = locate_object(image_bytes, target)
+
+    # ── Fallback: PulsePointNet (indoor-object ontology, ~45 classes) ──
+    if result is None:
+        result = predict(image_bytes, target_name=target if target else None)
+
     result["latency_ms"] = round((time.time() - start) * 1000)
-
     return result
-
 
 
 # ── Text / Tea CNN endpoints ─────────────────────────────────────────────────
@@ -54,14 +67,6 @@ async def classify_text_endpoint(
     text: str = Body(..., embed=True, description="Tea description or spoken query"),
     top_k: int = Body(3, embed=True, description="Number of alternative tea types to return"),
 ):
-    """
-    Run TeaTextCNN on a text string.
-
-    Returns tea type, flavor profile, quality tier, and a 256-dim sentence embedding.
-    The model auto-trains on first call if no checkpoint exists (~5 seconds).
-
-    Example body: {"text": "gyokuro shade-grown umami marine vegetal", "top_k": 3}
-    """
     if not text or not text.strip():
         return JSONResponse(status_code=422, content={"error": "text must be non-empty"})
     if len(text) > 512:
@@ -76,9 +81,8 @@ async def classify_text_endpoint(
 
 @app.get("/tea-schema")
 async def tea_schema():
-    """Return the label ontology used by the TextCNN."""
     return {
-        "tea_types":    TEA_TYPES,
+        "tea_types":     TEA_TYPES,
         "flavor_labels": FLAVOR_LABELS,
         "quality_tiers": QUALITY_TIERS,
     }
@@ -86,7 +90,6 @@ async def tea_schema():
 
 @app.post("/retrain-text")
 async def retrain_text():
-    """Re-train the TeaTextCNN from scratch (runs synchronously, ~5–15 s on CPU)."""
     start = time.time()
     info = retrain()
     info["duration_ms"] = round((time.time() - start) * 1000)

tanushappapogu-max/CongAPP

server/main.py
@@ -18,6 +18,7 @@ try:
         validate_image_headers,
         validate_target,
     )
+    from .locate_model import locate_object, is_available as locate_available
     from .model import predict, INDOOR_OBJECT_ONTOLOGY as INDOOR_OBJECTS
     from .text_model import classify_text, retrain
     from .tea_dataset import TEA_TYPES, FLAVOR_LABELS, QUALITY_TIERS
@@ -34,11 +35,12 @@ except ImportError:  # Supports `uvicorn main:app` from the server directory.
         validate_image_headers,
         validate_target,
     )
+    from locate_model import locate_object, is_available as locate_available
     from model import predict, INDOOR_OBJECT_ONTOLOGY as INDOOR_OBJECTS
     from text_model import classify_text, retrain
     from tea_dataset import TEA_TYPES, FLAVOR_LABELS, QUALITY_TIERS
 
-app = FastAPI(title="Pulse Point Vision + Tea Text API")
+app = FastAPI(title="Pulse Point Vision API")
 
 app.add_middleware(
     CORSMiddleware,
@@ -108,6 +110,8 @@ async def health():
         "apiVersion": "v1",
         "objects": len(INDOOR_OBJECTS),
         "detector": metadata,
+        "locateAnything": locate_available(),
+        "fallbackObjects": len(INDOOR_OBJECTS),
     }
 
 
@@ -167,12 +171,21 @@ async def detect(
     except ValueError as error:
         return JSONResponse(status_code=400, content={"error": str(error)})
 
-    try:
-        result = predict(image_bytes, target_name=normalized_target or None)
-    except (UnidentifiedImageError, OSError, ValueError) as error:
-        return JSONResponse(status_code=400, content={"error": "Malformed image."})
+    result = None
 
-    # Keep the safety boundary authoritative even if a future model adapter
+    # ── Primary: LocateAnything-3B (open-vocabulary, any target) ──
+    if normalized_target:
+        result = locate_object(image_bytes, normalized_target)
+
+    # ── Fallback: PulsePointNet (indoor-object ontology, ~45 classes) ──
+    if result is None:
+        try:
+            result = predict(image_bytes, target_name=normalized_target or None)
+        except (UnidentifiedImageError, OSError, ValueError) as error:
+            return JSONResponse(status_code=400, content={"error": "Malformed image."})
+
+    # Keep the safety boundary authoritative even if a model adapter (either
+    # the PulsePointNet fallback or the LocateAnything-3B primary path)
     # returns incomplete or overly optimistic metadata.
     response_metadata = detector_metadata()
     if isinstance(result.get("metadata"), dict):
@@ -187,9 +200,7 @@ async def detect(
     result["assistiveReadyProof"] = False
     result["proof"] = False
     result["latency_ms"] = round((time.time() - start) * 1000)
-
     return result
-
 
 
 # ── Text / Tea CNN endpoints ─────────────────────────────────────────────────
@@ -199,14 +210,6 @@ async def classify_text_endpoint(
     text: str = Body(..., embed=True, description="Tea description or spoken query"),
     top_k: int = Body(3, embed=True, description="Number of alternative tea types to return"),
 ):
-    """
-    Run TeaTextCNN on a text string.
-
-    Returns tea type, flavor profile, quality tier, and a 256-dim sentence embedding.
-    The model auto-trains on first call if no checkpoint exists (~5 seconds).
-
-    Example body: {"text": "gyokuro shade-grown umami marine vegetal", "top_k": 3}
-    """
     if not text or not text.strip():
         return JSONResponse(status_code=422, content={"error": "text must be non-empty"})
     if len(text) > 512:
@@ -221,9 +224,8 @@ async def classify_text_endpoint(
 
 @app.get("/tea-schema")
 async def tea_schema():
-    """Return the label ontology used by the TextCNN."""
     return {
-        "tea_types":    TEA_TYPES,
+        "tea_types":     TEA_TYPES,
         "flavor_labels": FLAVOR_LABELS,
         "quality_tiers": QUALITY_TIERS,
     }
@@ -231,7 +233,6 @@ async def tea_schema():
 
 @app.post("/retrain-text")
 async def retrain_text():
-    """Re-train the TeaTextCNN from scratch (runs synchronously, ~5–15 s on CPU)."""
     start = time.time()
     info = retrain()
     info["duration_ms"] = round((time.time() - start) * 1000)